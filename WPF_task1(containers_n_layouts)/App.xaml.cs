c411bca68db9182ca0d7a17703c5b3cbd98237e4
vladp_d3fqm45 <vladp_d3fqm45@DESKTOP-FQLK9B1>
2023-07-14 19:19:14 +0200

    Add project files.

---
+++ WPF_task1(containers_n_layouts)/App.xaml.cs
@@ -0,0 +1,13 @@
+﻿using System.Configuration;
+using System.Data;
+using System.Windows;
+
+namespace WPF_task1_containers_n_layouts_
+{
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App : Application
+    {
+    }
+}
